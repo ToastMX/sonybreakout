e6cfe680de964c27d58235aafaebfedeedee833e
ToastMX <toastmx@twitter.com>
2017-07-11 18:44:39 +0200

    Blockcollision step1, blocks haben jetzt state

--- Block.java
+++ Block.java
@@ -1,6 +1,6 @@
 public class Block {
 
-	int  xPos, yPos, xSize, ySize, row, column;
+	int  xPos, yPos, xSize, ySize, state, row, column;
 	static int rectCount;
 
 	public Block (int xPos, int yPos, int xSize, int ySize){
@@ -8,5 +8,9 @@ public class Block {
 		this.yPos = yPos;
 		this.xSize = xSize;
 		this.ySize = ySize;
+		
+		// 1: normal 0:gone
+		this.state = 1;
 	}	
+
 }
